d3862d34d41fcd59aa0f686819adce176c0663c3
PugFather <80226202+PugFather@users.noreply.github.com>
2021-11-05 16:44:00 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+// const logo = document.querySelector('.header__img');
+
+// logo.addEventListener('load', function () {
+//   setInterval(function () {
+//     logo.classList.add('bounce');
+//   }, 3000);
+// });
+
+// const animate = document.querySelectorAll('.animate');
+
+const animate = document.querySelectorAll('.animate');
+
+const showAnimation = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.dataset.animate === 'left' &&
+    entry.target.classList.remove('fade--left');
+
+  entry.target.dataset.animate === 'down' &&
+    entry.target.classList.remove('fade--down');
+
+  entry.target.dataset.animate === 'in' &&
+    entry.target.classList.remove('fade--in');
+
+  observer.unobserve(entry.target);
+};
+
+const animateObserver = new IntersectionObserver(showAnimation, {
+  root: null,
+  threshold: 0.15,
+});
+
+animate.forEach(function (el) {
+  el.dataset.animate === 'left' && el.classList.add('fade--left');
+
+  el.dataset.animate === 'down' && el.classList.add('fade--down');
+
+  el.dataset.animate === 'in' && el.classList.add('fade--in');
+  animateObserver.observe(el);
+});
